2da80049d2ffc37581a7b1bad28810d7dd6b3a9d
woals4815 <71315300+woals4815@users.noreply.github.com>
2020-11-05 17:12:41 +0900

    Finally I made BLOCKCHAINNNN!!!!!

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 import * as CryptoJs from "crypto-js";
 
 class Block {
-
     static calculateBlockHash = (
         index:number, 
         previousHash:string, 
         timestamp:number, 
         data:string
-    ) :string => CryptoJs.SHA256(index + previousHash + timestamp + data).toString(); 
+    ) : string => 
+        CryptoJs.SHA256(index + previousHash + timestamp + data).toString(); 
 
     static validateStructure = (aBlock: Block): boolean =>
         typeof aBlock.index === "number" &&
-        typeof aBlock.hash === "string"&&
+        typeof aBlock.hash === "string" &&
         typeof aBlock.previousHash === "string" &&
-        typeof aBlock.timestamp === "string" &&
+        typeof aBlock.timestamp === "number" &&
         typeof aBlock.data === "string";
 
     public index: number;
@@ -65,17 +65,42 @@ const createNewBlock = (data: string): Block => {
         data,
         newTimestamp
     );
+    addBlock(newBlock);
     return newBlock;
 };
 
-const isBlockvaild = (candidateBlock: Block, previousBlock: Block): boolean => {
+const getHashforBlock = (aBlock: Block): string =>
+    Block.calculateBlockHash(
+        aBlock.index,
+        aBlock.previousHash,
+        aBlock.timestamp,
+        aBlock.data
+    );
+
+const isBlockVaild = (candidateBlock: Block, previousBlock: Block): boolean => {
     if(!Block.validateStructure(candidateBlock)) {
         return false;
     } else if (previousBlock.index + 1 !== candidateBlock.index) {
         return false;
     } else if (previousBlock.hash !== candidateBlock.previousHash) {
         return false;
+    } else if (getHashforBlock(candidateBlock)!== candidateBlock.hash) {
+        return false;
+    } else {
+        return true;
     }
 };
+
+const addBlock = (candidateBlock: Block): void => {
+    if (isBlockVaild(candidateBlock, getLatestBlock())) {
+        blockchain.push(candidateBlock);
+    }
+};
+
+createNewBlock("second Block");
+createNewBlock("third Block");
+createNewBlock("fourth Block");
+
+console.log(blockchain);
 
 export {};
