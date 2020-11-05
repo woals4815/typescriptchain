e00f651e5c8ca5a05f132882446a656347608204
woals4815 <71315300+woals4815@users.noreply.github.com>
2020-11-05 14:07:01 +0900

    re-install using 'yarn add typescript' command line

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const name = "Jayden", age = 25, gender = "Male";
-const sayHi = (name, age, gender) => {
-    console.log(`Hello ${name}. You are ${age}years old. And you are ${gender}`);
-};
-sayHi(name, age);
-//# sourceMappingURL=index.js.map
