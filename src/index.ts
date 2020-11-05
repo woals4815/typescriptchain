30d99e266bf1cca47b5ca612487585637d0566cb
woals4815 <71315300+woals4815@users.noreply.github.com>
2020-11-05 14:22:46 +0900

    Make & Use interface

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+interface Human {
+    name: string;
+    age: number;
+    gender: string;
+}
+
+const person = {
+    name: "Jayden",
+    age: 25,
+    gender: "Male"
 };
 
-console.log(sayHi("Jayden", 25, "male"));
+const sayHi = (person: Human): string => {
+    return `hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
+};
+
+console.log(sayHi(person));
 
 export {};
