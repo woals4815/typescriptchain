524c37fbb87f4ab8e5de4e07d8752a048866a911
woals4815 <71315300+woals4815@users.noreply.github.com>
2020-11-05 16:28:40 +0900

    Made static validation function & calculateHash function

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,28 @@
+import * as CryptoJs from "crypto-js";
+
 class Block {
+
+    static calculateBlockHash = (
+        index:number, 
+        previousHash:string, 
+        timestamp:number, 
+        data:string
+    ) :string => CryptoJs.SHA256(index + previousHash + timestamp + data).toString(); 
+
+    static validateStructure = (aBlock: Block): boolean =>
+        typeof aBlock.index === "number" &&
+        typeof aBlock.hash === "string"&&
+        typeof aBlock.previousHash === "string" &&
+        typeof aBlock.timestamp === "string" &&
+        typeof aBlock.data === "string";
+
     public index: number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
+
     constructor(
         index: number,
         hash: string,
@@ -21,8 +40,42 @@ class Block {
 
 const genesisBlock: Block =  new Block(0, "20202020202", "", "Hello", 123455);
 
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
 
-console.log(blockchain);
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length -1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+    const previousBlock: Block = getLatestBlock();
+    const newIndex: number = previousBlock.index + 1;
+    const newTimestamp: number = getNewTimeStamp();
+    const newHash: string = Block.calculateBlockHash(
+        newIndex,
+        previousBlock.hash,
+        newTimestamp,
+        data
+    );
+    const newBlock: Block = new Block(
+        newIndex,
+        newHash,
+        previousBlock.hash,
+        data,
+        newTimestamp
+    );
+    return newBlock;
+};
+
+const isBlockvaild = (candidateBlock: Block, previousBlock: Block): boolean => {
+    if(!Block.validateStructure(candidateBlock)) {
+        return false;
+    } else if (previousBlock.index + 1 !== candidateBlock.index) {
+        return false;
+    } else if (previousBlock.hash !== candidateBlock.previousHash) {
+        return false;
+    }
+};
 
 export {};
